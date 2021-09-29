d48fb998cbb133a62dbe3da1c12630fa083658a3
voiciphil <voiciphil@gmail.com>
2021-09-29 20:04:48 +0900

    chore: 프로젝트 시작

---
+++ src/main/kotlin/com/voiciphil/sbwk/SbwkApplication.kt
@@ -0,0 +1,11 @@
+package com.voiciphil.sbwk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SbwkApplication
+
+fun main(args: Array<String>) {
+	runApplication<SbwkApplication>(*args)
+}
---
+++ src/test/kotlin/com/voiciphil/sbwk/SbwkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.voiciphil.sbwk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SbwkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

8afc454d16d839364b835880452d7355e453a040
voiciphil <voiciphil@gmail.com>
2021-10-01 23:29:16 +0900

    feat: JobParameter와 simpleStep2 추가

--- src/main/kotlin/com/voiciphil/sbwk/job/SimpleJobConfiguration.kt
+++ src/main/kotlin/com/voiciphil/sbwk/job/SimpleJobConfiguration.kt
@@ -3,30 +3,48 @@ package com.voiciphil.sbwk.job
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.lang.RuntimeException
 
 @Configuration
 class SimpleJobConfiguration(
     val jobBuilderFactory: JobBuilderFactory,
     val stepBuilderFactory: StepBuilderFactory
 ) {
-    @Bean("simpleJob")
+    @Bean
     fun simpleJob(): Job {
         return jobBuilderFactory.get("simpleJob")
-            .start(simpleStep1())
+            .start(simpleStep1(null))
+            .next(simpleStep2(null))
             .build()
     }
 
-    @Bean("simpleStep1")
-    fun simpleStep1(): Step {
+    @Bean
+    @JobScope
+    fun simpleStep1(@Value("#{jobParameters[requestDate]}") requestDate: String?): Step {
         return stepBuilderFactory.get("simpleStep1")
             .tasklet { _, _ ->
                 println("simpleStep1")
+                println("requestDate: $requestDate")
                 RepeatStatus.FINISHED
             }
             .build();
+    }
+
+    @Bean
+    @JobScope
+    fun simpleStep2(@Value("#{jobParameters[requestDate]}") requestDate: String?): Step {
+        return stepBuilderFactory.get("simpleStep2")
+            .tasklet { _, _ ->
+                println("simpleStep2")
+                println("requestDate: $requestDate")
+                RepeatStatus.FINISHED
+            }
+            .build()
     }
 }
